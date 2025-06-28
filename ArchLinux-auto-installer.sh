0a312861660afdd16b354a9c09d23fca801a906b
Mohamad Badiezadegan <mbzadegan@gmail.com>
2025-06-28 10:54:37 -0700

    Update ArchLinux-auto-installer.sh

--- ArchLinux-auto-installer.sh
+++ ArchLinux-auto-installer.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-# --- SETTINGS ---
+# --- CONFIGURATION ---
 DISK="/dev/sda"
 HOSTNAME="arch"
 ROOT_PASSWORD="changeme"
 
-echo "[*] Wiping $DISK"
+echo "[*] Wiping $DISK and creating partitions"
 sgdisk -Z "$DISK"
 sgdisk -n 1:0:+512M -t 1:ef00 -c 1:EFI "$DISK"
 sgdisk -n 2:0:0     -t 2:8300 -c 2:ROOT "$DISK"
@@ -20,29 +20,50 @@ mount ${DISK}2 /mnt
 mkdir /mnt/boot
 mount ${DISK}1 /mnt/boot
 
-echo "[*] Installing base system"
-pacstrap /mnt base linux linux-firmware
+echo "[*] Installing base system with networking and SSH"
+pacstrap /mnt base linux linux-firmware openssh systemd-networkd systemd-resolved vim sudo
 
 echo "[*] Generating fstab"
 genfstab -U /mnt >> /mnt/etc/fstab
 
-echo "[*] Configuring system in chroot"
+echo "[*] Chrooting and configuring system"
 arch-chroot /mnt /bin/bash <<EOF
+# Timezone and locale
 ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 hwclock --systohc
-
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+# Hostname and hosts
 echo "$HOSTNAME" > /etc/hostname
+cat <<HOSTS > /etc/hosts
+127.0.0.1   localhost
+::1         localhost
+127.0.1.1   $HOSTNAME.localdomain $HOSTNAME
+HOSTS
 
-echo "127.0.0.1 localhost" >> /etc/hosts
-echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 $HOSTNAME.localdomain $HOSTNAME" >> /etc/hosts
-
+# Root password
 echo "root:$ROOT_PASSWORD" | chpasswd
 
-echo "[*] Installing bootloader"
+# Enable systemd services
+systemctl enable systemd-networkd
+systemctl enable systemd-resolved
+systemctl enable sshd
+
+# Configure simple DHCP for Ethernet
+cat <<NET > /etc/systemd/network/20-wired.network
+[Match]
+Name=en*
+
+[Network]
+DHCP=yes
+NET
+
+# Link resolv.conf
+ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+
+# Install and configure GRUB (UEFI)
 pacman -S --noconfirm grub efibootmgr
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
